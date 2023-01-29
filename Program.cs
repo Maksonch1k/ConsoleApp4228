f091c5bbb3907fd6b8c5e425d5a29e4a6de2c70a
Hi-tech <Hi-tech@DESKTOP-K8PC2B7>
2023-01-29 19:44:24 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp4228
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int count = 0;
+            //for (int x = 10; x < 99; x++)
+            //{
+            //    if (x % 2 == 0)
+            //    {
+
+            //        Console.WriteLine(x);
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"count= {count}");
+
+
+            int summ = 0;
+            for (int x = 10; x <= 99; x++)
+            {
+                if (x % 25 == 0)
+                {
+                    summ += x;
+                }
+
+            }
+            Console.WriteLine($"{summ}");
+            Console.Read();
+                    
+        }
+    }
+}
